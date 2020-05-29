14568674d751798ea09bd5b4922aab8cbbfa56b8
Dawid <dawidbury1998@gmail.com>
2020-05-29 19:07:16 +0200

    Add serializer to seances

--- src/seance/seance.controller.ts
+++ src/seance/seance.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, Controller, Get, Param, ParseIntPipe, UseInterceptors } from '@nestjs/common';
 import { SeanceService } from './seance.service';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Seance } from './seance.entity';
@@ -9,12 +9,14 @@ export class SeanceController {
     constructor(private seanceService: SeanceService) {}
 
     @Get()
+    @UseInterceptors(ClassSerializerInterceptor)
     @ApiResponse({ status: 200, description: 'Array of seances objects', type: Seance })
     async getSeances(): Promise<Seance[]> {
         return await this.seanceService.getSeances();
     }
 
     @Get('/:id')
+    @UseInterceptors(ClassSerializerInterceptor)
     async getSeanceById(@Param('id', ParseIntPipe) id: number): Promise<Seance> {
         return await this.seanceService.getSeanceById(id);
     }
